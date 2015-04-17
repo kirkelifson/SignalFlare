1a6590cb2ce1f805210dd48a0a4ddc34a2cfaa64
Kirk Elifson <kirk@parodybit.net>
2015-04-17 15:19:33 -0400

    v1.0.3: Double quotes for interp. Error handling.

--- lib/SignalFlare.rb
+++ lib/SignalFlare.rb
@@ -1,5 +1,11 @@
-require 'open-uri'
-require 'cloudflare'
+require "open-uri"
+require "cloudflare"
+
+class RecordNotFound < Exception
+end
+
+class RecordUnchanged < Exception
+end
 
 class SignalFlare
   def initialize(api_key, email)
@@ -9,7 +15,7 @@ class SignalFlare
   end
 
   def update_ip(hostname)
-    split = hostname.partition('.')
+    split = hostname.partition(".")
     @host, @domain = split[0], split[2]
 
     begin
@@ -17,31 +23,33 @@ class SignalFlare
       dns_ip = nil
       external_ip = fetch_ip()
 
-      @api.rec_load_all(@domain)['response']['recs']['objs'].each do |record|
-        if record['name'] == hostname
-          record_id = record['rec_id']
-          dns_ip = record['content']
+      @api.rec_load_all(@domain)["response"]["recs"]["objs"].each do |record|
+        if record["name"] == hostname
+          record_id = record["rec_id"]
+          dns_ip = record["content"]
           break
         end
       end
 
       if record_id == nil
-        puts 'Suitable record for ' + hostname + ' not found.'
+        raise RecordNotFound
       end
 
       if dns_ip == external_ip
-        puts 'IP for ' + hostname + ' has not changed.'
+        raise RecordUnchanged
       end
 
-      @api.rec_edit(@domain, 'A', record_id, hostname, external_ip, 1)
+      @api.rec_edit(@domain, "A", record_id, hostname, external_ip, 1)
 
-      puts 'IP for ' + hostname + ' has been updated from' + dns_ip + ' to ' + external_ip
-    rescue => e
-      puts e.message
+      puts "IP for #{hostname} has been updated from #{dns_ip} to #{external_ip}"
+    rescue RecordNotFound
+      puts "Suitable record for #{hostname} not found."
+    rescue RecordUnchanged
+      puts "IP for #{hostname} has not changed."
     end
   end
 
   def fetch_ip
-    open('http://api.ipify.org').read
+    open("http://api.ipify.org").read
   end
 end
