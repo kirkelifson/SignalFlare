3088e1e6886d0b3a86b0a330f019b3a9466f2a80
Kirk Elifson <kirk@parodybit.net>
2015-04-20 17:02:29 -0400

    v1.1: fixed newline error, now printing responses

--- lib/SignalFlare.rb
+++ lib/SignalFlare.rb
@@ -26,21 +26,21 @@ class SignalFlare
     end
 
     if record_id == nil
-      puts "Suitable record for #{hostname} not found."
+      print "Suitable record for #{hostname} not found."
       return
     end
 
     if dns_ip == external_ip
-      puts "IP for #{hostname} has not changed."
+      print "IP for #{hostname} has not changed. (#{external_ip})"
       return
     end
 
     @api.rec_edit(domain, "A", record_id, hostname, external_ip, 1)
 
-    puts "IP for #{hostname} has been updated from #{dns_ip} to #{external_ip}"
+    print "IP for #{hostname} has been updated from #{dns_ip} to #{external_ip}"
   end
 
   def fetch_ip
-    open("http://curlmyip.com").read
+    open("http://curlmyip.com").read.chop
   end
 end
