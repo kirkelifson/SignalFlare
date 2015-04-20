ed9da6af7eb7bde1fe7849084bf485c53fe00c48
Kirk Elifson <kirk@parodybit.net>
2015-04-20 16:47:37 -0400

    stuff

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ gem 'cloudflare', '>= 2.0'
 gem 'SignalFlare', '>= 1.0.3'
 
 group :test do
+  gem 'rspec', '>= 2.8.0'
   gem 'rake'
 end
--- lib/SignalFlare.rb
+++ lib/SignalFlare.rb
@@ -1,55 +1,46 @@
 require "open-uri"
 require "cloudflare"
 
-class RecordNotFound < Exception
-end
-
-class RecordUnchanged < Exception
-end
-
 class SignalFlare
   def initialize(api_key, email)
-    @api_key = api_key
-    @email = email
-    @api = CloudFlare::connection(api_key, email)
+    self.api_key = api_key
+    self.email = email
+    self.api = CloudFlare::connection(api_key, email)
   end
 
   def update_ip(hostname)
     split = hostname.partition(".")
-    @host, @domain = split[0], split[2]
+    host = split[0]
+    domain = split[2]
 
-    begin
-      record_id = nil
-      dns_ip = nil
-      external_ip = fetch_ip()
+    record_id = nil
+    dns_ip = nil
+    external_ip = fetch_ip()
 
-      @api.rec_load_all(@domain)["response"]["recs"]["objs"].each do |record|
-        if record["name"] == hostname
-          record_id = record["rec_id"]
-          dns_ip = record["content"]
-          break
-        end
+    self.api.rec_load_all(domain)["response"]["recs"]["objs"].each do |record|
+      if record["name"] == hostname
+        record_id = record["rec_id"]
+        dns_ip = record["content"]
+        break
       end
-
-      if record_id == nil
-        raise RecordNotFound
-      end
-
-      if dns_ip == external_ip
-        raise RecordUnchanged
-      end
-
-      @api.rec_edit(@domain, "A", record_id, hostname, external_ip, 1)
-
-      puts "IP for #{hostname} has been updated from #{dns_ip} to #{external_ip}"
-    rescue RecordNotFound
-      puts "Suitable record for #{hostname} not found."
-    rescue RecordUnchanged
-      puts "IP for #{hostname} has not changed."
     end
+
+    if record_id == nil
+      puts "Suitable record for #{hostname} not found."
+      return
+    end
+
+    if dns_ip == external_ip
+      puts "IP for #{hostname} has not changed."
+      return
+    end
+
+    self.api.rec_edit(domain, "A", record_id, hostname, external_ip, 1)
+
+    puts "IP for #{hostname} has been updated from #{dns_ip} to #{external_ip}"
   end
 
   def fetch_ip
-    open("http://api.ipify.org").read
+    open("http://curlmyip.com").read
   end
 end
