b0f32f33251d037db2e777f8c922d90252ae56e6
Kirk Elifson <kirk@parodybit.net>
2015-04-21 22:06:27 -0400

    example

---
+++ test.rb
@@ -0,0 +1,17 @@
+# Usage:
+# $ cat ~/.rvm/environments/(current ruby version) > ~/.bashrc-rvm
+# $ crontab -u (user) -e
+# SHELL=/bin/bash
+# BASH_ENV=/home/(user)/.bashrc-rvm
+# 0 0 * * * /home/(user)/.rvm/rubies/(current ruby version)/bin/ruby /path/to/script/location.rb
+
+#!/usr/bin/env ruby
+
+require "./lib/SignalFlare.rb"
+
+hostname = "test.parodybit.net"
+api_key = "not_a_real_api_key"
+email = "kirk@parodybit.net"
+
+cloudflare = SignalFlare.new(api_key, email)
+puts cloudflare.update_ip(hostname)
